d4df132f00abee09ac3bec525405e95754aab3ab
Victor S <13732684+victorsundaram@users.noreply.github.com>
2018-07-20 11:02:52 -0400

    update

--- bot.js
+++ bot.js
@@ -15,7 +15,7 @@ console.log('Bot server started in the ' + process.env.NODE_ENV + ' mode');
 
 bot.on('message', (msg) => {
   const name = msg.from.first_name;
-  bot.sendMessage(msg.chat.id, 'Hello, ' + name + '!').then(() => {
+  bot.sendMessage(msg.chat.id, 'Hello, Asshole ' + name + '!').then(() => {
     // reply sent!
   });
 });
